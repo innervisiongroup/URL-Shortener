5e8ffb0cc765167c735f8665b5d2da80a127b3e2
Clement Rigo <clementr@innervisiongroup.com>
2014-09-23 17:07:29 +0200

    improving template with button

--- app/views/links/create.blade.php
+++ app/views/links/create.blade.php
@@ -1,18 +1,27 @@
 @extends('layout')
 
 @section('content')
-<div class="put-the-dang-thing-in-the-middle">
-    <h1>Shorten a URL</h1>
-
-    {{ Form::open(['url' => 'api/v1/url']) }}
-        <div class="form-group">
-            {{ Form::text('url', null, ['class' => 'form-control', 'id' => 'shorten-input', 'placeholder' => 'https://innervisiongroup.com', 'autofocus'=>'autofocus']) }}
-            {{ $errors->first('url', '<div class="error">:message</div>') }}
+<div class="container">
+    <div class="row put-the-dang-thing-in-the-middle">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="">
+                <h1>Shorten a URL</h1>
+            
+                {{ Form::open(['url' => 'api/v1/url']) }}
+                    <div class="input-group">
+                        {{ Form::text('url', null, ['class' => 'form-control', 'id' => 'shorten-input', 'placeholder' => 'http://innervisionweb.com', 'autofocus'=>'autofocus']) }}
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="submit">Go!</button>
+                        </span>
+                    </div>
+                    {{ $errors->first('url', '<div class="error">:message</div>') }}
+                {{ Form::close() }}
+            
+                @if (Session::has('hashed'))
+                    <output>{{ link_to(Session::get('hashed')) }}</output>
+                @endif
+            </div>
         </div>
-    {{ Form::close() }}
-
-    @if (Session::has('hashed'))
-        <output>{{ link_to(Session::get('hashed')) }}</output>
-    @endif
+    </div>
 </div>
 @stop
