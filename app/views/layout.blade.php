a3a32900c66416ec908cee518d4d39b82eafef28
Clement Rigo <clementr@innervisiongroup.com>
2014-09-23 16:55:59 +0200

    use bootstrap3.2

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Little Shortener</title>
     <link rel="stylesheet" href="fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700">
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
